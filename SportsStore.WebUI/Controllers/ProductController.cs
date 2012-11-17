70a53b0aa507f54a9baa7a3dfd711a2ab9d24daf
dramsey14820 <dramsey14820@gmail.com>
2012-11-17 12:21:41 -0600

    Set up mock data framework.
    Created repository interface.
    
    Created a product view.

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        #region Private members
+
+        private IProductRepository repository;
+
+        #endregion
+
+        #region Constructors
+
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+
+        #endregion
+
+    }
+}
--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -2,6 +2,11 @@
 using System.Web.Mvc;
 using System.Web.Routing;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using System.Linq;
+using Moq;
+using System.Collections.Generic;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -38,6 +43,16 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // TODO:  Put additional bindings here!!!
+
+            // Mock implementation of the IProductRepository Interface
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+                {
+                    new Product {Name="Football", Price = 25 },
+                    new Product {Name="Surf Board", Price = 179 },
+                    new Product {Name="Running Shoes", Price = 95 }
+                }.AsQueryable());
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
 
         #endregion
